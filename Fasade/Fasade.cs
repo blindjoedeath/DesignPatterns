50effa5e64fd43a4bfb87c54593d7eefbaebe445
Оскар Моисеев <blindjoedeath0@gmail.com>
2019-01-29 14:40:21 +0300

    Added fasade

---
+++ Fasade/Fasade.cs
@@ -0,0 +1,78 @@
+﻿namespace SandBox.Patterns.Fasade
+{
+    using System;
+    
+    public class DatabaseFasade: IDisposable
+    {
+        private DataBase _dataBase;
+        private BaseConnecter _baseConnecter;
+        
+        public DatabaseFasade()
+        {
+            _dataBase = new DataBase("/Data/");
+            _baseConnecter = new BaseConnecter(_dataBase);
+        }
+        
+        public object LoadData()
+        {
+            if (!_baseConnecter.IsConnected)
+            {
+                _baseConnecter.Connect();
+            }
+
+            return _baseConnecter.Load();
+        }
+        
+        public void SaveData(object data)
+        {
+            if (!_baseConnecter.IsConnected)
+            {
+                _baseConnecter.Connect();
+            }
+            _baseConnecter.Save(data);
+        }
+
+        public void Dispose()
+        {
+            _baseConnecter.Disconnect(); 
+        }
+    }
+    
+    public class DataBase
+    {
+        public DataBase(string path)
+        {
+            
+        }
+    }
+
+    public class BaseConnecter
+    {
+        public bool IsConnected { get; set; }
+        public BaseConnecter(DataBase dataBase)
+        {
+            
+        }
+
+        public void Connect()
+        {
+            IsConnected = true;
+        }
+
+        public object Load()
+        {
+            return null;
+        }
+        
+        public void Save(object data)
+        {
+            
+        }
+
+        public void Disconnect()
+        {
+            IsConnected = false;
+        }
+        
+    }
+}
